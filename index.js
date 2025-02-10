88d1b6c83b262c9f16eb2d3e8a9cf3c6e6fce176
НАТАЛИ <natalya.osetrova1980@gmail.com>
2025-02-10 16:37:16 +0300

    доработка кода

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger-menu");
+    const nav = document.querySelector(".nav");
+
+    burger.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+});
